6e62a03f42a6526d302c54a8c00931d285b63d61
Tim Finnell <tim.finnell@gmail.com>
2012-02-11 23:34:28 -0800

    whitespace formatting

--- gitnotify.rb
+++ gitnotify.rb
@@ -15,19 +15,19 @@ AMQP.start(broker) do |connection|
 
   queue.subscribe do |md, pl|
 
-		jpl = JSON.parse(pl)
-		message = jpl["payload"]["message"]
-		id = jpl["payload"]["id"]
-		repo = jpl["_meta"]["routing_key"].split(".")[3]
-		branch = jpl["_meta"]["routing_key"].split(".")[4]
-		author = jpl["payload"].has_key?("author") ?
-			jpl["payload"]["author"]["username"] : "Something"
-	  head = "#{author} pushed to #{repo}/#{branch}"
+    jpl = JSON.parse(pl)
+    message = jpl["payload"]["message"]
+    id = jpl["payload"]["id"]
+    repo = jpl["_meta"]["routing_key"].split(".")[3]
+    branch = jpl["_meta"]["routing_key"].split(".")[4]
+    author = jpl["payload"].has_key?("author") ?
+      jpl["payload"]["author"]["username"] : "Something"
+    head = "#{author} pushed to #{repo}/#{branch}"
 
-		puts [message, id, head]
-		Libnotify.show(body: message,
-									 summary: head,
-									 timeout: 10,
-									 icon_path: "/usr/share/icons/github/github-icon.png")
-	end
+    puts [message, id, head]
+    Libnotify.show(body: message,
+                   summary: head,
+                   timeout: 10,
+                   icon_path: "/usr/share/icons/github/github-icon.png")
+  end
 end
