e01dec6bf2394c628537bac0e0d6b74940d74b65
Tim Finnell <tim.finnell@gmail.com>
2012-02-13 02:00:44 -0800

    enabled channel auto-recovery

--- gitnotify.rb
+++ gitnotify.rb
@@ -9,30 +9,19 @@ $0 = "gitnotify\0"
 
 class Gitnotify
   def initialize(subscription, options, connection)
-    channel = AMQP::Channel.new(connection)
+    channel = AMQP::Channel.new(connection, auto_recovery: true)
     queue = channel.queue(subscription, durable: true)
 
-    connection.on_tcp_connection_loss do |connection, settings|
-      puts "tcp connection loss"
-    end
-
-    connection.on_connection_interruption do |connection|
-      puts "Connection interruption?"
-    end
-
-    channel.on_error do |ch, channel_close|
-      puts channel_close.reply_text
-      connection.close { EM.stop }
-    end
-
     queue.subscribe do |md, pl|
+
       begin
         jpl = JSON.parse(pl)
         icon = options[:icon]
-      rescue
+      rescue JSON::ParserError
         jpl = JSON.parse '{"payload":{"message":"someones sending bad JSON :(","id":"error","author":{"username": "system"}},"_meta":{ "routing_key":"system.error.fed.bad.json"}}'
         icon =  Dir["/home/tim/git/trollicons/Icons/#{["Sad","Rage"].sample}/*"].sample
       end
+      
       message = jpl["payload"]["message"]
       id = jpl["payload"]["id"]
       repo = jpl["_meta"]["routing_key"].split(".")[3]
@@ -60,7 +49,13 @@ SUBSCRIPTIONS = YAML.load_file('config/subscription.yml')
 broker = SUBSCRIPTIONS["github"][:cred]
 
 AMQP.start(broker) do |connection|
-  puts "Connected to AMQP broker..."
+  product = connection.server_properties["product"] 
+  version = connection.server_properties["version"]
+  user = connection.settings[:user]
+  host = connection.settings[:host]
+  port = connection.settings[:port]
+
+  puts " #{user} connected to #{host}:#{port} running #{product} version #{version}..."
 
   SUBSCRIPTIONS.each do |subscription, options|
     Gitnotify.new(subscription, options[:notification], connection)
