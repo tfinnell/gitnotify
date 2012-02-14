fa6f86acd4f0b7d81d52e59dd320a1ff73df5703
Tim Finnell <tim.finnell@gmail.com>
2012-02-13 17:29:10 -0800

    fuuuuuuuuuuuuuuuuuuuuuuu!!!!!!!!1

--- gitnotify.rb
+++ gitnotify.rb
@@ -16,44 +16,41 @@ AMQP.start(broker) do |connection|
   product = connection.server_properties["product"] 
   version = connection.server_properties["version"]
   user = connection.settings[:user]
+  options = SUBSCRIPTIONS["github"]
 
   puts " #{user} on #{connection.broker_endpoint} running #{product} version #{version}..."
 
-  SUBSCRIPTIONS.each do |subscription, options|
-    channel = AMQP::Channel.new(connection, auto_recovery: true)
-    queue = channel.queue(subscription, durable: true)
+  channel = AMQP::Channel.new(connection, auto_recovery: true)
+  queue = channel.queue("github", durable: true)
 
-    Signal.trap('INT') do
-      AMQP.stop { EM.stop { exit } }
-      puts ""
+  Signal.trap('INT') do
+    AMQP.stop { EM.stop { exit } }
+    puts ""
+  end
+
+  queue.subscribe do |md, pl|
+
+    begin
+      jpl = JSON.parse(pl)
+      icon = options[:notification][:icon]
+    rescue JSON::ParserError
+      jpl = JSON.parse '{"payload":{"message":"someones sending bad JSON :(","id":"error","author":{"username": "system"}},"_meta":{ "routing_key":"system.error.fed.bad.json"}}'
+      icon =  Dir["/home/tim/git/trollicons/Icons/#{["Sad","Rage"].sample}/*"].sample
     end
+    
+    message = jpl["payload"]["message"]
+    id = jpl["payload"]["id"]
+    repo = jpl["_meta"]["routing_key"].split(".")[3]
+    branch = jpl["_meta"]["routing_key"].split(".")[4]
+    author = jpl["payload"].has_key?("author") ?
+      jpl["payload"]["author"]["username"] : "Something"
+    head = "#{author} pushed to #{repo}/#{branch}"
 
-    puts options
-
-    queue.subscribe do |md, pl|
-
-      begin
-        jpl = JSON.parse(pl)
-        icon = options[:notification][:icon]
-      rescue JSON::ParserError
-        jpl = JSON.parse '{"payload":{"message":"someones sending bad JSON :(","id":"error","author":{"username": "system"}},"_meta":{ "routing_key":"system.error.fed.bad.json"}}'
-        icon =  Dir["/home/tim/git/trollicons/Icons/#{["Sad","Rage"].sample}/*"].sample
-      end
-      
-      message = jpl["payload"]["message"]
-      id = jpl["payload"]["id"]
-      repo = jpl["_meta"]["routing_key"].split(".")[3]
-      branch = jpl["_meta"]["routing_key"].split(".")[4]
-      author = jpl["payload"].has_key?("author") ?
-        jpl["payload"]["author"]["username"] : "Something"
-      head = "#{author} pushed to #{repo}/#{branch}"
-
-      puts "#{head}: #{message}"
-      Libnotify.show(
-        summary: head,
-        body: message,
-        timeout: options[:notification][:timeout],
-        icon_path: icon)
-    end
+    puts "#{head}: #{message}"
+    Libnotify.show(
+      summary: head,
+      body: message,
+      timeout: options[:notification][:timeout],
+      icon_path: icon)
   end
 end
