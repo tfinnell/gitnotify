9aab8ebb4a1a08ff847e5b24bacc4609680f26ef
Tim Finnell <tim.finnell@gmail.com>
2012-02-13 17:00:19 -0800

    got rid of the class and methods etc...

--- gitnotify.rb
+++ gitnotify.rb
@@ -1,39 +1,34 @@
 #!/usr/bin/env ruby
 
 require 'amqp'
-require 'libnotify'
 require 'json'
 require 'yaml'
+require 'libnotify'
 require 'pry'
 
 $0 = "gitnotify\0"
 
-class Gitnotify
-  def initialize(subscription, options, connection)
+SUBSCRIPTIONS = YAML.load_file('config/subscription.yml')
+
+broker = SUBSCRIPTIONS["github"][:cred]
+
+AMQP.start(broker) do |connection|
+  product = connection.server_properties["product"] 
+  version = connection.server_properties["version"]
+  user = connection.settings[:user]
+
+  puts " #{user} on #{connection.broker_endpoint} running #{product} version #{version}..."
+
+  SUBSCRIPTIONS.each do |subscription, options|
     channel = AMQP::Channel.new(connection, auto_recovery: true)
     queue = channel.queue(subscription, durable: true)
 
-    EM::PeriodicTimer.new 30.0 do
-      puts "#{Time.now} - still alive... "
+    Signal.trap('INT') do
+      AMQP.stop { EM.stop { exit } }
+      puts ""
     end
 
     queue.subscribe do |md, pl|
-
-      connection.on_connection_interruption do
-        puts "connection interruption"
-      end
-
-      connection.on_tcp_connection_failure do
-        puts "connection tcp connection failure"
-      end
-
-      connection.on_tcp_connection_loss do
-        puts "tcp connection loss"
-      end
-
-      channel.on_error do
-        puts "channel error"
-      end
 
       begin
         jpl = JSON.parse(pl)
@@ -52,31 +47,11 @@ class Gitnotify
       head = "#{author} pushed to #{repo}/#{branch}"
 
       puts "#{head}: #{message}"
-      notification({
+      Libnotify.show(
         summary: head,
         body: message,
         timeout: options[:timeout],
-        icon_path: icon})
+        icon_path: icon)
     end
-  end
-
-  def notification(gnotify)
-    Libnotify.show(gnotify)
-  end
-end
-
-SUBSCRIPTIONS = YAML.load_file('config/subscription.yml')
-
-broker = SUBSCRIPTIONS["github"][:cred]
-
-AMQP.start(broker) do |connection|
-  product = connection.server_properties["product"] 
-  version = connection.server_properties["version"]
-  user = connection.settings[:user]
-
-  puts " #{user} on #{connection.broker_endpoint} running #{product} version #{version}..."
-
-  SUBSCRIPTIONS.each do |subscription, options|
-    Gitnotify.new(subscription, options[:notification], connection)
   end
 end
